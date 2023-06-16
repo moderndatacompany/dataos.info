43d7fd573530e185debf99727f2e714dded4c483
pyjs-tmdc <piyush.joshi@tmdc.io>
2023-06-16 22:46:35 +0530

    added path convertor script + workflow update

---
+++ notion-path-convertor/constants.py
@@ -0,0 +1,4 @@
+# Replace the placeholder with path to your Notion exported .zip file
+notion_zip_file_path = {{specify-the-path-to-your-.zip-file}}
+# Replace the placeholder with path where you want to extract the contents
+extract_to = {{specify-the-folder-where-you-want-to-extract-the-contents}}
