e19d9e875317d5a008c409669c71ab8827aaa347
Nanda <nanda.page@tmdc.io>
2023-06-26 20:13:39 +0530

    Added content to Atlas section and subpages

--- notion-path-convertor/dev/rex.py
+++ notion-path-convertor/dev/rex.py
@@ -8,8 +8,8 @@ def process_link(match):
     print(link)
     print("---")
 
-    # Replace '%20' with '_' and lowercase the content in parentheses
-    processed_link = re.sub(r'\((.*?)\)', lambda m: '(' + m.group(1).replace('%20', '_').lower() + ')', link)
+    # Replace '_' with '_' and lowercase the content in parentheses
+    processed_link = re.sub(r'\((.*?)\)', lambda m: '(' + m.group(1).replace('_', '_').lower() + ')', link)
     print(processed_link)
     print("---")
 
@@ -49,11 +49,11 @@ markdown = """
 
 This is a Markdown file containing links.
 
-[****Depot Config Templates****](Depot%20bf0fbe4ed0e84b9098c391e7caee2f42/Depot%20Config%20Templates%20c26b0f2c441c4c55a1c3c23831667787.md)
+[****Depot Config Templates****](Depot_bf0fbe4ed0e84b9098c391e7caee2f42/Depot_Config_Templates_c26b0f2c441c4c55a1c3c23831667787.md)
 
 [Storage](https://www.notion.so/Storage-fb33f69575ad46aca3c35a80aa233caa?pvs=21)
 
-![Flow when Hive is chosen as the catalog type](Depot%20bf0fbe4ed0e84b9098c391e7caee2f42/depot_catalog.png)
+![Flow when Hive is chosen as the catalog type](Depot_bf0fbe4ed0e84b9098c391e7caee2f42/depot_catalog.png)
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 
