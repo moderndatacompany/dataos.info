6d7db7a58a681632e99dfb744af060f758f77ac7
Piyush Joshi <piyush.joshi@tmdc.io>
2025-02-06 13:32:50 +0530

    Updating docs on Mintlify

--- notion_path_convertor/constants.py
+++
@@ -1,4 +0,0 @@
-# Replace the placeholder with path to your Notion exported .zip file
-notion_zip_file_path = "/Users/nandapage/metis.zip"
-# Replace the placeholder with path where you want to extract the contents
-extract_to = "/Users/nandapage/metis/"
